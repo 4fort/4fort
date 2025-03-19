6605fa321efb87efa4600f791693822e75de445c
4Fort <dexterfortbsilva@gmail.com>
2025-03-19 18:31:47 +0800

    Fetch top programming languages and update README generation logic

--- userfetch.js
+++ userfetch.js
@@ -51,24 +51,65 @@ async function fetchCommitCount() {
     .totalContributions;
 }
 
+async function fetchTopLanguages() {
+  const query = `
+    {
+      user(login: "${username}") {
+        repositories(first: 100, isFork: false) {
+          nodes {
+            languages(first: 10, orderBy: { field: SIZE, direction: DESC }) {
+              edges {
+                size
+                node {
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const response = await graphqlWithAuth(query);
+  const repos = response.user.repositories.nodes;
+
+  const languageMap = new Map();
+
+  repos.forEach((repo) => {
+    repo.languages.edges.forEach(({ size, node }) => {
+      const { name } = node;
+      if (languageMap.has(name)) {
+        languageMap.set(name, languageMap.get(name) + size);
+      } else {
+        languageMap.set(name, size);
+      }
+    });
+  });
+
+  // Sort languages by total size in descending order and get the top 10
+  const topLanguages = [...languageMap.entries()]
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 10)
+    .map(([name]) => name);
+
+  return topLanguages.join(", ");
+}
+
 async function updateReadme() {
-  const [stars, commits] = await Promise.all([
+  const [stars, commits, languages] = await Promise.all([
     fetchStarCount(),
     fetchCommitCount(),
+    fetchTopLanguages(),
   ]);
 
-  let templateContent = await fs.readFile("README.TEMPLATE.md", "utf8");
+  let templateContent = await fs.readFile("README.template.md", "utf8");
 
   // Replace placeholders with actual values
-  const placeholders = {
-    "{{ STARS }}": stars,
-    "{{ COMMITS }}": commits,
-  };
-
-  let readmeContent = templateContent.replace(
-    /{{\s*[\w]+\s*}}/g,
-    (match) => placeholders[match] || match
-  );
+  let readmeContent = templateContent
+    .replace("{{ STARS }}", stars)
+    .replace("{{ COMMITS }}", commits)
+    .replace("{{ LANGUAGES }}", languages);
 
   await fs.writeFile("README.md", readmeContent);
 }
